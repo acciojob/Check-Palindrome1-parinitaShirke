b040d13f7865eee811068ae395060e396b52ab2c
parinitaShirke <131198150+parinitaShirke@users.noreply.github.com>
2023-08-12 00:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	let ch="";
+	for(int i=(str.length-1);i>0;i--){
+		ch+=str[i];
+	}
+	if(ch==str){
+		return true;
+	}else{
+		return false;
+	}
 
 }
 module.exports = palindrome
